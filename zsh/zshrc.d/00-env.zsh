4b6d06926e47acd70d0288e5c026bd7e9bb5a9b1
Shanazar <snizamaddinow@gmail.com>
2026-04-22 17:11:27 +0300

    add mcfly

--- zsh/zshrc.d/00-env.zsh
+++ zsh/zshrc.d/00-env.zsh
@@ -4,17 +4,26 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 export PATH="$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH"
 export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
+
 export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_ENV_HINTS=1
+
 export ZSH_DISABLE_COMPFIX=true
 export DISABLE_UPDATE_PROMPT=YES
-export EDITOR=nvim
 
+export EDITOR=nvim
 unset MAILCHECK
+
+export MCFLY_FUZZY=2
+export MCFLY_KEY_SCHEME=vim
+export MCFLY_RESULTS=6
+export MCFLY_DELETE_WITHOUT_CONFIRM=true
+export MCFLY_INTERFACE_VIEW=BOTTOM
+export MCFLY_HISTORY_LIMIT=1000
+export MCFLY_DISABLE_MENU=TRUE
 
 # zsh options
 setopt auto_cd
 setopt auto_pushd
 setopt pushd_ignore_dups
 setopt pushdminus
-
-
--- zsh/zshrc.d/70-keybindings.zsh
+++ zsh/zshrc.d/70-keybindings.zsh
@@ -4,8 +4,5 @@ select-word-style bash
 
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
-bindkey -M vicmd 'k' history-substring-search-up
-bindkey -M vicmd 'j' history-substring-search-down
 bindkey '^[^?' backward-kill-word
-bindkey '^R' history-incremental-search-backward
 
